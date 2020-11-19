b48aa70e90578a3ff3dfdb96996729efcff77b7c
schevail <schevail@cst-tp-b10110>
2020-11-19 11:36:58 +0100

    Test des heures planifiées

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -1,5 +1,6 @@
 package champollion;
 
+import java.util.Date;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -45,5 +46,23 @@ public class ChampollionJUnitTest {
             assertEquals(10 + 15, untel.heuresPrevues());
             untel.ajouteEnseignement(uml, 0, 0, 20);
             assertEquals(10 + 15 + 15, untel.heuresPrevues());
+        }
+        
+        @Test 
+        public void testHeuresPlanifiees(){
+            Salle salle = new Salle("Salle", 30);
+            Date date = new Date("19/11/2020");
+            Intervention interv = new Intervention(date, 20, TypeIntervention.TP, salle);
+            untel.ajouteIntervention(interv);
+            assertEquals(15, untel.heuresPlanifiees());
+            Intervention interv2 = new Intervention(date, 10, TypeIntervention.CM, salle);
+            untel.ajouteIntervention(interv2);
+            assertEquals(15 + 15, untel.heuresPlanifiees());
+            Intervention interv3 = new Intervention(date, 10, TypeIntervention.TD, salle);
+            untel.ajouteIntervention(interv3);
+            assertEquals(40, untel.heuresPlanifiees());
+            Intervention interv4 = new Intervention(date, 0, null, salle);
+            untel.ajouteIntervention(interv4);
+            assertEquals(40, untel.heuresPlanifiees());
         }
 }
